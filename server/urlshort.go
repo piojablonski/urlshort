acb12a879291433b140f5a51904e3b048fc94679
pjablonski <piojablonski@gmail.com>
2023-02-01 15:11:32 +0100

    Add ability to use a fallback handler when url is missing in a map

--- server/urlshort.go
+++ server/urlshort.go
@@ -3,17 +3,14 @@ package server
 import "net/http"
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
-}
-
-func DefaultMapHandler(pathsToUrls map[string]string) http.HandlerFunc {
-
 	return func(writer http.ResponseWriter, request *http.Request) {
 		reqUrl := request.URL.Path
-		val := pathsToUrls[reqUrl]
-		writer.WriteHeader(http.StatusMovedPermanently)
-
-		writer.Header().Set("Location", val)
+		val, exists := pathsToUrls[reqUrl]
+		if exists {
+			writer.WriteHeader(http.StatusMovedPermanently)
+			writer.Header().Set("Location", val)
+		} else {
+			fallback.ServeHTTP(writer, request)
+		}
 	}
 }
--- server/urlshort_test.go
+++ server/urlshort_test.go
@@ -12,6 +12,14 @@ var redirects = map[string]string{
 	"/tests-for-http": "https://quii.gitbook.io/learn-go-with-tests/build-an-application/http-server#http.listenandserve-5000-...",
 }
 
+type SpyHandler struct {
+	calledServeHttp bool
+}
+
+func (s *SpyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	s.calledServeHttp = true
+}
+
 func TestMapHandler(t *testing.T) {
 	t.Run("when called with known url, it the handler redirect to it", func(t *testing.T) {
 
@@ -19,8 +27,17 @@ func TestMapHandler(t *testing.T) {
 		req, _ := http.NewRequest(http.MethodGet, url, nil)
 		res := httptest.NewRecorder()
 
-		server.DefaultMapHandler(redirects).ServeHTTP(res, req)
+		server.MapHandler(redirects, &SpyHandler{}).ServeHTTP(res, req)
 		assert.Equal(t, res.Code, http.StatusMovedPermanently)
 		assert.Equal(t, res.Header().Get("Location"), redirects[url])
+	})
+
+	t.Run("uses fallback handler when path doesn't match any url in a map", func(t *testing.T) {
+		url := "/not-existing-path"
+		req, _ := http.NewRequest(http.MethodGet, url, nil)
+		res := httptest.NewRecorder()
+		fallback := &SpyHandler{}
+		server.MapHandler(redirects, fallback).ServeHTTP(res, req)
+		assert.True(t, fallback.calledServeHttp)
 	})
 }
